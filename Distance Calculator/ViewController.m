b86b19a35dfe3cfabbe69ad7c72f10c630dbb615
Kevin Skompinski <kevons5252@gmail.com>
2017-05-17 20:12:28 -0400

    Initial Commit

--- Distance Calculator/ViewController.m
+++ Distance Calculator/ViewController.m
@@ -8,20 +8,100 @@
 
 #import "ViewController.h"
 
+#import "DistanceGetter/DGDistanceRequest.h"
+
 @interface ViewController ()
+
+@property (nonatomic) DGDistanceRequest *req;
+
+@property (weak, nonatomic) IBOutlet UITextField *StartingLocation;
+
+@property (weak, nonatomic) IBOutlet UITextField *EndLocation1;
+
+@property (weak, nonatomic) IBOutlet UITextField *EndLocation2;
+
+@property (weak, nonatomic) IBOutlet UITextField *EndLocation3;
+
+@property (weak, nonatomic) IBOutlet UITextField *EndLocation4;
+
+@property (weak, nonatomic) IBOutlet UILabel *Distance1;
+
+@property (weak, nonatomic) IBOutlet UILabel *Distance2;
+
+@property (weak, nonatomic) IBOutlet UILabel *Distance3;
+
+@property (weak, nonatomic) IBOutlet UILabel *Distance4;
+
+@property (weak, nonatomic) IBOutlet UIButton *UpdateButton;
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)ButtonTapped:(id)sender {
+    
+    self.UpdateButton.enabled = NO;
+    self.req = [DGDistanceRequest alloc];
+    
+    NSString *start = self.StartingLocation.text;
+    NSString *firstD = self.EndLocation1.text;
+    NSString *secondD = self.EndLocation2.text;
+    NSString *thirdD = self.EndLocation3.text;
+    NSString *fourthD = self.EndLocation4.text;
+    
+    NSArray *dests = @[firstD,secondD,thirdD,fourthD];
+    self.req = [self.req initWithLocationDescriptions:dests sourceDescription:start];
+    
+    __weak ViewController *weakSelf = self;
+    
+    self.req.callback = ^(NSArray *responses){
+        ViewController *strongSelf = weakSelf;
+        if(!strongSelf) return;
+        
+        NSNull *badResult = [NSNull null];
+        
+        if(responses[0] != badResult){
+            double num = ([responses[0] floatValue]/1000.0);
+            NSString *x = [NSString stringWithFormat:@"%.2f km", num];
+            strongSelf.Distance1.text = x;
+        }
+        else{
+            strongSelf.Distance1.text = @"Error";
+        }
+        
+        if(responses[1] != badResult){
+            double num = ([responses[1] floatValue]/1000.0);
+            NSString *x = [NSString stringWithFormat:@"%.2f km", num];
+            strongSelf.Distance2.text = x;
+        }
+        else{
+            strongSelf.Distance2.text = @"Error";
+        }
+        
+        if(responses[2] != badResult){
+            double num = ([responses[2] floatValue]/1000.0);
+            NSString *x = [NSString stringWithFormat:@"%.2f km", num];
+            strongSelf.Distance3.text = x;
+        }
+        else{
+            strongSelf.Distance3.text = @"Error";
+        }
+        
+        if(responses[3] != badResult){
+            double num = ([responses[3] floatValue]/1000.0);
+            NSString *x = [NSString stringWithFormat:@"%.2f km", num];
+            strongSelf.Distance4.text = x;
+        }
+        else{
+            strongSelf.Distance4.text = @"Error";
+        }
+        
+        strongSelf.req = nil;
+        strongSelf.UpdateButton.enabled = YES;
+    };
+    
+    [self.req start];
+    
 }
 
 @end
